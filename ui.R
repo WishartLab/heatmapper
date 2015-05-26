8deb6c8682f3325784f78bb7c8abb54b9099c5b4
Sasha Babicki <sbabicki@gmail.com>
2015-05-26 13:48:40 -0600

    switched to shiny tutorial example instead

--- ui.R
+++ ui.R
@@ -8,11 +8,21 @@ shinyUI(navbarPage(
 	tabPanel(title = "Microarray"), 
 	
 	#### Continuous Maps Panel ####
-	tabPanel(title = "Continuous Maps"), 
+	tabPanel(title = "Continuous Maps", 
+		sidebarLayout(
+    		sidebarPanel(fileInput("cmFile", label = h3("File input"))),
+			mainPanel(
+				tabsetPanel(type = "tabs", 
+					tabPanel(title = "Plot", plotOutput("continuousMap")), 
+					tabPanel(title = "Table", dataTableOutput("continuousTable")))))), 
 	
 	#### Discrete Maps Panel ####
 	tabPanel(title = "Discrete Maps", 
-		plotOutput(outputId='discreteMap')
-		)
+		sidebarLayout(
+    		sidebarPanel(fileInput("dmFile", label = h3("File input"))),
+			mainPanel(
+				tabsetPanel(type = "tabs", 
+					tabPanel(title = "Plot", plotOutput("discreteMap")), 
+					tabPanel(title = "Table", dataTableOutput("discreteTable"))))))
 	
 ))
