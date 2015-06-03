347e8b4fd3b00e17b653720ee0f3d7db8c26f3e2
Sasha Babicki <sbabicki@gmail.com>
2015-06-03 10:35:54 -0600

    added example for continuous from http://stackoverflow.com/questions/22289794/read-shape-file-with-readogr-verses-readshapepoly

--- server.R
+++ server.R
@@ -3,6 +3,12 @@ library(maps)
 library(mapproj)
 library(ctc)
 source("helpers.R")
+
+library(maptools)
+library(rgdal)         # for readOGR(...)
+library(ggplot2)
+library(RColorBrewer)  # for brewer.pal(...)
+
 options(shiny.deprecation.messages=FALSE)
 
 shinyServer(function(input, output, session){
@@ -82,9 +88,23 @@ shinyServer(function(input, output, session){
 	
 	#### CONTINUOUS MAP ####
 	output$continuousMap <- renderPlot({
+		sample <- data.frame(Longitude=c(-1+rnorm(50,0,.5),-2+rnorm(50,0,0.5),-4.5+rnorm(50,0,.5)),
+		                     Latitude =c(52+rnorm(50,0,.5),54+rnorm(50,0,0.5),56+rnorm(50,0,.5)))
+		UKmap  <- readOGR(dsn="data/GBR_am/",layer="GBR_adm2")
+		map.df <- fortify(UKmap)
+		
+		ggplot(sample, aes(x=Longitude, y=Latitude)) + 
+		  stat_density2d(aes(fill = ..level..), alpha=0.5, geom="polygon")+
+		  #geom_point(colour="red")+
+		  geom_path(data=map.df,aes(x=long, y=lat,group=group), colour="grey50")+
+		  scale_fill_gradientn(colours=rev(brewer.pal(7,"Spectral")))+
+		  xlim(-10,+2.5) +
+		  coord_fixed()
 	})
 	
 	output$continuousTable <- renderDataTable({
+		data.frame(Longitude=c(-1+rnorm(50,0,.5),-2+rnorm(50,0,0.5),-4.5+rnorm(50,0,.5)),
+		                     Latitude =c(52+rnorm(50,0,.5),54+rnorm(50,0,0.5),56+rnorm(50,0,.5)))
 	})
 	
 	get_dmFile <- function(){
