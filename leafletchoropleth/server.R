fae04398e6ef4c95de5d93aea729f2b72ee1ab32
Sasha Babicki <sbabicki@gmail.com>
2015-06-29 11:10:16 -0600

    adding reactive functions to leaflet choropleth

--- leafletchoropleth/server.R
+++ leafletchoropleth/server.R
@@ -4,43 +4,46 @@ library(maps)
 
 # source: https://jcheng.shinyapps.io/choropleth3/
 shinyServer(function(input, output, session) {
+  
+	values <- reactiveValues(highlight = c())
 	
-
-	get_density <- function(){
+	density <- reactive({
 		data_file <- get_file()
 		name_col <- data_file[[1]]
 		nums_col <- data_file[[2]]
 		names(nums_col) <- name_col
 		density <- c(nums_col)
 		return(density)
+	})
+	
+	get_spec <- function(x){
+		data_file <- get_file()
+		name_col <- data_file[[1]]
+		nums_col <- data_file[[2]]
+		names(nums_col) <- name_col
+		density2 <- c(nums_col)
+		print(grep(pattern = x, x = name_col))
+		return(density2[x])
 	}
 	
-	get_file <- function(){
-		
-		data_file <- read.table("data/statetest2.txt", header = TRUE, sep="\t")
-		data_file[,1] <- tolower(data_file[,1])
-
-		return(data_file)
-		
+	get_file <- observe({
 		if(input$chooseInput == 'example'){
 			path <- "data/counties.rds"
 			counties <- readRDS(path)
 		}
 		else{
-		#	validate(need(input$file$datapath, "Please upload a file"))
-		#	counties <- read.delim(input$file$datapath, header = TRUE)
+			#validate(need(input$file$datapath, "Please upload a file"))
+			#counties <- read.delim(input$file$datapath, header = TRUE)
 			counties <- read.table("data/statetest2.txt", header = TRUE, sep="\t")
 			counties[,1] <- tolower(counties[,1])
 		}
-
 		return(counties)
-	}
+	})
 	
-	density <- get_density()
-	
-
+	#density <- get_density()
+all <- reactive({
 	# Breaks we'll use for coloring
-	densityBreaks <- round(seq(min(density, na.rm = TRUE), max(density, na.rm = TRUE), length.out = 9), 0)
+	densityBreaks <- round(seq(min(density, na.rm = TRUE), max(density, na.rm = TRUE), length.out = 9), 0) 
 
 	# Construct break ranges for displaying in the legend
 	densityRanges <- data.frame(
@@ -50,24 +53,20 @@ shinyServer(function(input, output, session) {
 
 	# Eight colors for eight buckets
 	palette <- colorRampPalette(c("#FFEDA0", "#800026"))(8)
-	#c("#FFEDA0", "#FED976", "#FEB24C", "#FD8D3C",
-   #          "#FC4E2A", "#E31A1C", "#BD0026", "#800026")
+		
 	# Assign colors to states
 	colors <- structure(
 	  palette[cut(density, densityBreaks)],
 	  names = tolower(names(density))
 	)
+	})
 
 	# The state names that come back from the maps package's state database has
 	# state:qualifier format. This function strips off the qualifier.
 	getStateName <- function(id) {
 	  strsplit(id, ":")[[1]][1]
 	}
-	
-  values <- reactiveValues(highlight = c())
-  
-  #map <- createLeafletMap(session, "map")
-	
+
 	get_map <- function(){
 		return(map("county",  plot=FALSE, fill=TRUE))
 	}
@@ -76,12 +75,12 @@ shinyServer(function(input, output, session) {
   # Draw the given states, with or without highlighting
   output$map <- renderLeaflet({
   	statesData <- map("state",  plot=FALSE, fill=TRUE)
-  
+  	#statesData <- get_map()
   	
   	get_style <- function(){
   		i <- 1
   		sarray <- rep("#000000", length(statesData$names))
-  		print(sarray)
+  		
   		for(s in statesData$names){
   			s <- strsplit(s, ":")[[1]][1]
   			tryCatch({
@@ -103,28 +102,9 @@ shinyServer(function(input, output, session) {
 		)
 
   	
-    leaflet() %>% addTiles() %>% addPolygons(statesData$x, statesData$y, statesData$names,weight = 0.5, color = "#000000", opacity = 1,
+    leaflet() %>% addTiles() %>% 
+  	addPolygons(statesData$x, statesData$y, statesData$names,weight = 1, color = "#000000", opacity = 1,
     	fillColor = statesData$fillColour, fillOpacity = 0.8)})
-  	#	I(states$y), I(states$x), I(states$names),
-     # I(lapply(states$names, function(x) {
-      #  x <- strsplit(x, ":")[[1]][1]
-      #	tryCatch({
-      #		list(fillColor = colors[[x]])
-      #	}, 
-      	# if the region doesn't match anything
-      #	error = function (err){
-      #		list(fillColor = "#000000")
-      #	})
-        
-      #})))
- # })
-
-  # session$onFlushed is necessary to delay the drawing of the polygons until
-  # after the map is created
-  session$onFlushed(once=TRUE, function() {
-    # Get shapes from the maps package
-    #drawStates(names(density))
-  })
   
   # input$map_shape_mouseover gets updated a lot, even if the id doesn't change.
   # We don't want to update the polygons and stateInfo except when the id
@@ -134,37 +114,18 @@ shinyServer(function(input, output, session) {
   observe({
     values$highlight <- input$map_shape_mouseover$id
   })
-	
-	testing <- reactive({
-		file <- get_file(input$chooseInput)
-		updateSelectInput(session, inputId="colSelect", choices = colnames(file)[-1], selected = input$colSelect)		
-		name_col <- file[[1]]
-		#print(name_col)
-		if(input$colSelect != "a"){
-			nums_col <- file[[input$colSelect]]
-		}
-		else{
-			nums_col <- file[[2]]
-		}
-		print(head(name_col, 1))
-		print(head(nums_col,1))
-		if(!is.null(nums_col)){
-			names(nums_col) <- name_col
-			density <<- c(nums_col)
-			densityBreaks <<- round(seq(min(density, na.rm = TRUE), max(density, na.rm = TRUE), length.out = 9), 0)
-		}
-	})
   
   # Dynamically render the box in the upper-right
   output$stateInfo <- renderUI({
     if (is.null(values$highlight)) {
       return(tags$div("Hover over a state"))
-    } else {
+    } 
+  	else {
       # Get a properly formatted state name
-      stateName <- names(density)[getStateName(values$highlight) == tolower(names(density))]
+      stateName <- getStateName(values$highlight)
       return(tags$div(
         tags$strong(stateName),
-        tags$div(density[stateName], HTML(""))
+        tags$div(get_spec(stateName), HTML(""))
       ))
     }
   })
