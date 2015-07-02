e7880c8fff100c5d0e81e1c3906cf4ab87da180e
Sasha Babicki <sbabicki@gmail.com>
2015-07-02 14:15:50 -0600

    still trying to get reactivity to work on choropleth

--- leafletchoropleth/server.R
+++ leafletchoropleth/server.R
@@ -4,29 +4,21 @@ library(maps)
 
 # source: https://jcheng.shinyapps.io/choropleth3/
 shinyServer(function(input, output, session) {
-  
-	values <- reactiveValues(highlight = c())
+
+  values <- reactiveValues(highlight = c())
 	
-	density <- reactive({
+	get_density <- function() { 
 		data_file <- get_file()
 		name_col <- data_file[[1]]
 		nums_col <- data_file[[2]]
 		names(nums_col) <- name_col
-		density <- c(nums_col)
-		return(density)
-	})
-	
-	get_spec <- function(x){
-		data_file <- get_file()
-		name_col <- data_file[[1]]
-		nums_col <- data_file[[2]]
-		names(nums_col) <- name_col
-		density2 <- c(nums_col)
-		print(grep(pattern = x, x = name_col))
-		return(density2[x])
+		print(nums_col)
+		print(names(nums_col))
+		#density <- c(nums_col)
+		return(nums_col)
 	}
 	
-	get_file <- observe({
+	get_file <- function(){
 		if(input$chooseInput == 'example'){
 			path <- "data/counties.rds"
 			counties <- readRDS(path)
@@ -38,44 +30,38 @@ shinyServer(function(input, output, session) {
 			counties[,1] <- tolower(counties[,1])
 		}
 		return(counties)
+	}
+	observe({
+		values$density <- get_density()
 	})
 	
-	#density <- get_density()
-all <- reactive({
 	# Breaks we'll use for coloring
-	densityBreaks <- round(seq(min(density, na.rm = TRUE), max(density, na.rm = TRUE), length.out = 9), 0) 
+	densityBreaks <- reactive({ round(seq(min(values$density, na.rm = TRUE), max(values$density), na.rm = TRUE, length.out = 9), 0) })
 
 	# Construct break ranges for displaying in the legend
-	densityRanges <- data.frame(
-	  from = head(densityBreaks, length(densityBreaks)-1),
-	  to = tail(densityBreaks, length(densityBreaks)-1)
-	)
+	# densityRanges <<- data.frame(
+	#  from = head(densityBreaks, length(densityBreaks)-1),
+	#  to = tail(densityBreaks, length(densityBreaks)-1)
+	# )
 
 	# Eight colors for eight buckets
 	palette <- colorRampPalette(c("#FFEDA0", "#800026"))(8)
 		
 	# Assign colors to states
-	colors <- structure(
-	  palette[cut(density, densityBreaks)],
-	  names = tolower(names(density))
-	)
-	})
+	colors <- reactive({ structure(
+	  palette[cut(values$density, densityBreaks)],
+	  names = tolower(names(values$density))
+	)})
 
 	# The state names that come back from the maps package's state database has
 	# state:qualifier format. This function strips off the qualifier.
 	getStateName <- function(id) {
 	  strsplit(id, ":")[[1]][1]
 	}
-
-	get_map <- function(){
-		return(map("county",  plot=FALSE, fill=TRUE))
-	}
 	
-
   # Draw the given states, with or without highlighting
   output$map <- renderLeaflet({
   	statesData <- map("state",  plot=FALSE, fill=TRUE)
-  	#statesData <- get_map()
   	
   	get_style <- function(){
   		i <- 1
@@ -87,7 +73,7 @@ all <- reactive({
   				sarray[[i]] <- colors[[s]]
   				}, 
   				error = function(e){
-  					print(s)
+  					#print("error")
   				})
   			i <- i + 1
   		}
@@ -101,7 +87,6 @@ all <- reactive({
 		  fillOpacity = 0.8
 		)
 
-  	
     leaflet() %>% addTiles() %>% 
   	addPolygons(statesData$x, statesData$y, statesData$names,weight = 1, color = "#000000", opacity = 1,
     	fillColor = statesData$fillColour, fillOpacity = 0.8)})
@@ -122,10 +107,10 @@ all <- reactive({
     } 
   	else {
       # Get a properly formatted state name
-      stateName <- getStateName(values$highlight)
+      stateName <- names(values$density)[getStateName(values$highlight) == tolower(names(values$density))]
       return(tags$div(
         tags$strong(stateName),
-        tags$div(get_spec(stateName), HTML(""))
+        tags$div(values$density[stateName], HTML(""))
       ))
     }
   })
