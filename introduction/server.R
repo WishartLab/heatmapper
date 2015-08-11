eadba40671335ca48e22debc68554261c73cbf55
Sasha Babicki <sbabicki@gmail.com>
2015-08-11 16:37:54 -0600

    created html files for instructions/gallery/contact

--- introduction/server.R
+++ introduction/server.R
@@ -1,3 +1,14 @@
 library(shiny)
 shinyServer(function(input, output, session){
+	output$instructions <- renderUI({
+		includeHTML(paste0("www/instructions/", input$navlistPanel, ".html"))
+	})
+	
+	output$gallery <- renderUI({
+		includeHTML(paste0("www/gallery/", input$navlistPanel, ".html"))
+	})
+	
+	output$contact <- renderUI({
+		includeHTML(paste0("www/contact/", input$navlistPanel, ".html"))
+	})
 })
--- introduction/ui.R
+++ introduction/ui.R
@@ -1,20 +1,31 @@
 source("../global_ui.R")
 
 shinyUI(fluidPage(
-	tags$head(
-		tags$style(".row {margin-left:0;}")), 
 	HEAD_TASKS("#introductionTab"),
 	
-	navlistPanel(id = "navlistPanel",
-		tabPanel("Introduction"), 
-		tabPanel("Microarray"),
-		tabPanel("Latitude/Longitude"),
-		tabPanel("Choropleth"),
-		tabPanel("Distance Matirx"),
-		tabPanel("Image Overlay"),
-	tabsetPanel(
-				tabPanel("Instructions"), 
-				tabPanel("Gallery"), 
-				tabPanel("Contact")
-			))
+	sidebarLayout(
+		sidebarPanel(id = "sidebarPanel",
+			navlistPanel(id = "navlistPanel", widths = c(12,12), well = FALSE,
+				tabPanel("Introduction", value = "introduction"),
+				tabPanel("Microarray", value = "microarray"),
+				tabPanel("Latitude/Longitude", value = "latitude_longitude"),
+				tabPanel("Choropleth", value = "choropleth"),
+				tabPanel("Distance Matirx", value = "distance_matrix"),
+				tabPanel("Image Overlay", value = "image")
+			)
+		),
+		mainPanel(id = "mainPanel",
+			tabsetPanel(
+				tabPanel("Instructions", 
+					tags$br(),
+					uiOutput("instructions")), 
+				tabPanel("Gallery", 
+					tags$br(),
+					uiOutput("gallery")), 
+				tabPanel("Contact", 
+					tags$br(),
+					uiOutput("contact"))
+			)
+		)
+	)
 ))
