4d3e7ae69f7993d383ddca9ca5d7d2b3fdecd2d6
Sasha Babicki <sbabicki@gmail.com>
2015-08-13 14:40:00 -0600

    trying out new section names: geomap and geodensity

--- about/global_about.R
+++ about/global_about.R
@@ -2,8 +2,8 @@ NAVLIST_PANEL <- function(){
 	navlistPanel(id = "navlistPanel", widths = c(12,12), well = FALSE,
 		tabPanel("Introduction", value = "introduction"),
 		tabPanel("Microarray", value = "microarray"),
-		tabPanel("Latitude/Longitude", value = "latitude_longitude"),
-		tabPanel("Choropleth", value = "choropleth"),
+		tabPanel("Geodensity", value = "latitude_longitude"),
+		tabPanel("Geomap", value = "choropleth"),
 		tabPanel("Distance Matirx", value = "distance_matrix"),
 		tabPanel("Image Overlay", value = "image")	
 	)
