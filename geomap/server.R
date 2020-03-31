c2c7bb4a036d784c87e46b2a36eb02f6b19aec3e
jliigand <43414731+liigandj@users.noreply.github.com>
2020-03-30 19:36:14 -0600

    updating validation
    Bug fix for correct validation if date is in the future or not

--- geomap/server.R
+++ geomap/server.R
@@ -1050,7 +1050,7 @@ shinyServer(function(input, output, session) {
     oldest_date <- min(dates_vec, na.rm = T)
 
     validate(
-      need(input$date < Sys.Date(), "Your selected date is in the future. Please select correct date") %then% #Error message for dates in the future
+      need(input$date <= Sys.Date(), "Your selected date is in the future. Please select correct date") %then% #Error message for dates in the future
       need(input$date >= oldest_date, 
            paste("No data available for this region on that date. \nWe can provide data for that region starting from",oldest_date)) %then% #Error message for dates that are too early for particular region
       need(!is.null(get_file()), "No data available for this region on that date") #Error message for data not available
