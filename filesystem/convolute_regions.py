a8e4e230abda4b3a550081b98e1bd8a1513d35a7
Zachary Budinski <zbudinsk@Zacharys-MacBook-Air.local>
2020-04-06 16:22:50 -0300

    Convolute regions for active case dat (where exist) confirmed diff elsewhere

--- filesystem/convolute_regions.py
+++ filesystem/convolute_regions.py
@@ -14,15 +14,15 @@ ontario_data =  [csv.reader(open(os.getcwd()+"/Global/North_America/Canada/Ontar
 canada_data =  [csv.reader(open(os.getcwd()+"/Global/North_America/Canada/accumulated.txt","rb"), delimiter = '\t'), "Canada"]
 us_data =  [csv.reader(open(os.getcwd()+"/Global/North_America/United_States_of_America/accumulated.txt","rb"), delimiter = '\t'), "United State of America"]
 cali_data = [csv.reader(open(os.getcwd()+"/Global/North_America/United_States_of_America/California/accumulated.txt","rb"), delimiter = '\t'), "California"]
-china_data = [csv.reader(open(os.getcwd()+"/Global/Asia/China/accumulated.txt","rb"), delimiter = '\t'), "China"]
+china_data = [csv.reader
+(open(os.getcwd()+"/Global/Asia/China/accumulated.txt","rb"), delimiter = '\t'), "China"]
 ns_data =  [csv.reader(open(os.getcwd()+"/Global/North_America/Canada/Nova_Scotia/accumulated.txt","rb"), delimiter = '\t'), "Nova Scotia"]
 italian_data = [csv.reader(open(os.getcwd()+"/Global/Europe/Italy/accumulated.txt","rb"), delimiter = '\t'), "Italy"]
 mexican_data =[csv.reader(open(os.getcwd()+"/Global/North_America/Mexico/accumulated.txt","rb"), delimiter = '\t'), "Mexico"]
-cases = [mexican_data]
+cases = [italian_data]
 
 def Extract(lst, index):
-    lst.remove("N/A")
-    return [float(item[index]) for item in lst[1:]] 
+    return [item[index] for item in lst[1:]] 
 
 def get_day_count(lst,value):
     days = []
@@ -45,37 +45,50 @@ def derivative_slope (lst):
         derive.append((lst[i]-lst[i-1])/lst[i])
     return np.array(derive)
 
-italy_curve = np.array(Extract(list(italy_data),5))
+
+italy_curve = np.array([float(item) for item in  Extract(list(italy_data),5)])
 italy_deriv_curve =  np.diff(italy_curve)/italy_curve[1:]
 italy_deriv_curve = np.convolve(medfilt(italy_deriv_curve), np.ones((5,))/5, mode='valid')
-
-plt.plot(np.array(get_day_count(italy_curve,0)), np.array(italy_curve), 'r-', label='projected')
-plt.xlabel("Day",fontsize=18)
-plt.ylabel("%  of New Cases",fontsize=16)
-plt.title("Italy Max Weighted New Cases Prediction",fontsize=22)
-plt.show()
-plt.plot(np.array(get_day_count(italy_deriv_curve,0)), np.array(italy_deriv_curve), 'r-', label='projected')
-plt.xlabel("Day",fontsize=18)
-plt.ylabel("Rate of Outbreak",fontsize=16)
-plt.title("Italy Max Weighted Outbreak Rate Prediction",fontsize=22)
-plt.show()
-
-
+# plt.plot(np.array(get_day_count(italy_curve,0)), np.array(italy_curve), 'r-', label='projected')
+# plt.xlabel("Day",fontsize=18)
+# plt.ylabel("%  of New Cases",fontsize=16)
+# plt.title("Italy Max Weighted New Cases Prediction",fontsize=22)
+# plt.show()
+# plt.plot(np.array(get_day_count(italy_deriv_curve,0)), np.array(italy_deriv_curve), 'r-', label='projected')
+# plt.xlabel("Day",fontsize=18)
+# plt.ylabel("Rate of Outbreak",fontsize=16)
+# plt.title("Italy Max Weighted Outbreak Rate Prediction",fontsize=22)
+# plt.show()
 for region, name in cases:
     data = list(region)
     region_confirmed = Extract(data,1)
-
     active_cases = Extract(data,4)
+    for item in region_confirmed:
+        if item == 'N/A':
+            region_confirmed.remove(item)
+    last_index = 0
 
-    region_confirmed = np.convolve(medfilt(np.diff(region_confirmed)), np.ones((3,))/3, mode='valid')
-    region_confirmed = np.convolve(medfilt(region_confirmed), np.ones((3,))/3, mode='valid')
+
+    region_confirmed = [float(item) for item in region_confirmed]
+
+    med_region_confirmed = np.convolve(medfilt(np.diff(region_confirmed)), np.ones((3,))/3, mode='valid')
+    med_region_confirmed = np.convolve(medfilt(med_region_confirmed), np.ones((3,))/3, mode='valid')
+
+    active_cases = filter(lambda a: a != 'N/A', active_cases)
+    active_cases = [float(item) for item in active_cases]
+    if all(v == 0.0 for v in active_cases):
+        active_cases = np.convolve(medfilt(region_confirmed), np.ones((3,))/3, mode='valid')
+    else:
+        active_cases = np.convolve(medfilt(active_cases), np.ones((3,))/3, mode='valid')
 
     region_curve = derivative_slope(active_cases)
-    region_curve = np.convolve(medfilt(active_cases), np.ones((3,))/3, mode='valid')
+    region_curve = np.convolve(medfilt(region_curve), np.ones((3,))/3, mode='valid')
     region_curve = derivative_slope(region_curve)
-    region_curve = np.convolve(medfilt(region_curve), np.ones((5,))/5, mode='valid')   
+    region_curve = np.convolve(medfilt(region_curve), np.ones((3,))/3, mode='valid')
+    region_curve = np.convolve(medfilt(region_curve), np.ones((5,))/5, mode='valid')
     region_curve = np.convolve(medfilt(region_curve), np.ones((7,))/7, mode='valid')
-    region_median = np.median(region_curve[-4:-1])
+    region_median = np.median(region_curve[-3:-1])
+
     i = 0
     day = 0
     closest_match = None
@@ -88,23 +101,21 @@ for region, name in cases:
                 closest_match = diff
                 day = i + 1
             i += 1
-
     projected_rates = italy_deriv_curve[day:]
     region_projected = []
     print projected_rates
+
     for rate in projected_rates:
         if not region_projected:
-            new_value = region_confirmed[-1]
+            new_value = med_region_confirmed[-1]
         else:
             new_value = region_projected[-1] + region_projected[-1]*rate
-        print rate
-        print new_value
         region_projected.append(new_value)
 
     plt.plot(np.array(get_day_count(region_curve,0)), np.array(region_curve), 'k--', label='outbreak rate')
     plt.show()
-    plt.plot(np.array(get_day_count(region_confirmed,0)), np.array(region_confirmed), 'k--', label='actual')
-    plt.plot(np.array(get_day_count(region_projected,len(region_confirmed))), np.array(region_projected), 'r-', label='projected')
+    plt.plot(np.array(get_day_count(med_region_confirmed,0)), np.array(med_region_confirmed), 'k--', label='actual')
+    plt.plot(np.array(get_day_count(region_projected,len(med_region_confirmed))), np.array(region_projected), 'r-', label='projected')
     plt.xlabel("Day",fontsize=18)
     plt.ylabel("New Cases",fontsize=16)
     plt.title(name,fontsize=22)
